f031b228834596c0e3f15ebcacad3a20055f5fc0
swhan9404 <swhan9404@naver.com>
2021-10-27 16:36:08 +0900

    ⭐️ feat: readme에 자동 블로그글 갱신

---
+++ update_readme.py
@@ -0,0 +1,71 @@
+import feedparser
+
+blog_rss_url = "https://v2.velog.io/rss/swhan9404"
+rss_feed = feedparser.parse(blog_rss_url)
+
+latest_blog_post_list = ""
+
+MAX_POST_NUM = 5
+
+for idx, feed in enumerate(rss_feed['entries']):
+    if idx > MAX_POST_NUM:
+        break
+    feed_date = feed['published_parsed']
+    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+
+markdown_text = """[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fswhan9404&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)           
+
+![header](https://capsule-render.vercel.app/api?type=waving&color=gradient&section=header&text=%20몰입하는개발자,한승운입니다👋%20&height=250&fontSize=50&animation=twinkling)
+
+
+<h3 align="center">⚒️ Tech Stack ⚒️</h3>
+<p align="center">
+  <img src="https://img.shields.io/badge/Java-palegoldenrod?style=flat-square&logo=Java&logoColor=red"/>
+  <img src="https://img.shields.io/badge/Spring-white?style=flat-square&logo=Spring&logoColor=white&color=6DB33F"/> &nbsp &nbsp 
+  
+  <img src="https://img.shields.io/badge/Python-3766AB?style=flat-square&logo=Python&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Django-092E20?style=flat-square&logo=Django&logoColor=white"/>&nbsp &nbsp 
+</p>  
+<p align="center">
+  <img src="https://img.shields.io/badge/Javascript-ffb13b?style=flat-square&logo=javascript&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Vue.js-4FC08D?style=flat-square&logo=Vue.js&logoColor=white"/>
+  
+</p>
+
+
+<h3 align="center"> 🍒 Me 🍒 </h3>
+<p align="center">
+  <a href="https://velog.io/@swhan9404/series">
+    <img src="https://img.shields.io/badge/Tech%20Blog-11B48A?style=flat-square&logo=Vimeo&logoColor=white&link=https://velog.io/@swhan9404/series"/>
+  </a>
+<!--   <a href="https://www.notion.so/419b1896ac1f46c28e7b2f78a33e3d9b">
+    <img src="https://img.shields.io/badge/%ED%8F%AC%ED%8A%B8%ED%8F%B4%EB%A6%AC%EC%98%A4-000000?style=flat-square&logo=Notion&logoColor=white&link=https://www.notion.so/419b1896ac1f46c28e7b2f78a33e3d9b"/>
+  </a> -->
+  <a href="mailto:gardener9404@gmail.com">
+    <img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=gardener9404@gmail.com"/>
+  </a>
+
+</p>
+
+
+
+<!--
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=swhan9404&layout=compact&theme=dracula)](https://github.com/metleeha)
+[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=swhan9404)](https://github.com/anuraghazra/github-readme-stats)
+[![Solved.ac
+프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=swhan9404)](https://solved.ac/swhan9404)
+-->
+<p align="center">
+  <a href="https://github.com/devxb/CommitCombo/">
+    <img src="http://commitcombo.com/get?user=swhan9404&theme=DeepOcean-mini"/>
+  </a>
+</p>
+
+
+<h2><img src="https://img.shields.io/badge/Tech%20Blog-11B48A?style=flat-square&logo=Vimeo&logoColor=white&link=https://velog.io/@swhan9404/series"/>My Latest Blog Post</h2> 
+"""
+
+readme_text = f"{markdown_text}{latest_blog_post_list}"
+
+with open("README.md", 'w', encoding='utf-8') as f:
+    f.write(readme_text)
